1e0dc37dea54e833bbf2cf019349a422811c50e0
Eugene Song <yougene42193@gmail.com>
2018-12-06 12:31:25 -0500

    cat drills

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-$('.thumbnail').
+function catClicks() {
+    $('.thumbnail').on('click',function(event) {
+      const targetCat = $(event.currentTarget).find('img').attr('src');
+      const otherCat = $(event.currentTarget).find('img').attr('alt');
+      $('.hero img').attr('src', targetCat).attr('alt', otherCat);
+    });
+  }
+  
+  $(catClicks)
